84bc36e525ebd98d9c9834ffa091f9fbfc1a7545
amish12822 <amishofficial23@gmail.com>
2024-07-27 13:43:00 +0530

    complete program check even or odd number

---
+++ even_odd.java
@@ -0,0 +1,15 @@
+/**
+ * even_odd
+ */
+public class even_odd {
+
+    public static void main(String[] args) {
+        int a = 21; // enter user value
+
+        if ( a%2 == 0) {
+            System.out.println(a + " is a Even Number.");
+        } else {
+            System.out.println(a + " is a Odd Number.");
+        }
+    }
+}
